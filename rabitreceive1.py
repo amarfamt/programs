10ca383200b1028e0d2d2ef6fc3bb15314f8ec3a
amarfamt <53889701+amarfamt@users.noreply.github.com>
2022-04-23 16:29:29 +0530

    for be it

--- rabitreceive1.py
+++ rabitreceive1.py
@@ -32,5 +32,5 @@ def callback(ch, method, properties, body):
 channel.basic_consume(callback,queue='ITAsystem',no_ack=True)
 channel.start_consuming()
 #testing 123456789
-#Just adding6
+#Just adding8
 #55
